846f72584f6ae3fa21a8323a9bdd32e74788b346
parv-sachdeva <parv.sachdeva@sonomabio.com>
2024-10-06 16:15:37 -0400

    add leaflet map module with ui and server

---
+++ R/leafletMap.R
@@ -0,0 +1,29 @@
+leafletMapUI <- function(id) {
+    ns <- NS(id)
+    tagList(
+        card(
+            card_header("Map", style="background-color:#008DE7;color:white;"),
+            leafletOutput(ns("map_plot")),
+        )
+    )
+}
+
+leafletMapServer <- function(id, biodiversity_data, country, animal) {
+    stopifnot(is.reactive(biodiversity_data))
+    stopifnot(is.reactive(country))
+    stopifnot(is.reactive(animal))
+    moduleServer(id, function(input, output, session) {
+        output$map_plot <- renderLeaflet({
+            req(biodiversity_data(), country(), animal())
+            leaflet(
+                biodiversity_data() %>% filter(scientificName==animal())
+            ) %>%            
+            addTiles() %>%
+            addMarkers(
+                ~longitudeDecimal, ~latitudeDecimal,
+                label = ~paste0(glue("Locality: {locality} <br> Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}")) %>% lapply(htmltools::HTML),
+                popup = ~paste0(glue("Locality: {locality}<br>Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}"))
+            )
+        })
+    })
+}
