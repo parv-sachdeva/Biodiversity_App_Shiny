84bb6f4a7f8304ba3d2e130b46556c46aae722b0
parv-sachdeva <parv.sachdeva@sonomabio.com>
2024-10-06 16:16:06 -0400

    Add tests

---
+++ tests/test.R
@@ -0,0 +1,20 @@
+library(glue)
+library(testthat)
+source("../R/utils.R")
+
+data_path="../data/split_by_country"
+
+test_that("Load all countries", {
+    countries <- get_all_countries(data_path = data_path)
+    expect_length(countries, 228)
+})
+
+test_that("Load countries from wrong path", {
+    countries <- get_all_countries(data_path = ".")
+    expect_null(countries)
+})
+
+test_that("Load and test Poland entries", {
+    data <- read_country_data(data_path = data_path, country = "Poland")
+    expect_equal(nrow(data), 48457)
+})
