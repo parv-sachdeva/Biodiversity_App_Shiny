9c859362682ee2dee3afd253bd8db8f41c922bd9
parv-sachdeva <parv.sachdeva@sonomabio.com>
2024-10-06 17:05:26 -0400

    Add tryCatch and additional filters

--- R/leafletMap.R
+++ R/leafletMap.R
@@ -15,15 +15,26 @@ leafletMapServer <- function(id, biodiversity_data, country, animal) {
     moduleServer(id, function(input, output, session) {
         output$map_plot <- renderLeaflet({
             req(biodiversity_data(), country(), animal())
-            leaflet(
-                biodiversity_data() %>% filter(scientificName==animal())
-            ) %>%            
-            addTiles() %>%
-            addMarkers(
-                ~longitudeDecimal, ~latitudeDecimal,
-                label = ~paste0(glue("Locality: {locality} <br> Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}")) %>% lapply(htmltools::HTML),
-                popup = ~paste0(glue("Locality: {locality}<br>Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}"))
-            )
+            tryCatch({
+                # Create a leaflet map
+                leaflet(
+                    biodiversity_data() %>% filter(
+                        scientificName==animal() &
+                        !is.na(latitudeDecimal) &
+                        !is.na(longitudeDecimal)
+                    )
+                ) %>%            
+                addTiles() %>%
+                addMarkers(
+                    ~longitudeDecimal, ~latitudeDecimal,
+                    label = ~paste0(glue("Locality: {locality} <br> Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}")) %>% lapply(htmltools::HTML),
+                    popup = ~paste0(glue("Locality: {locality}<br>Country: {country()}<br>Continent: {continent}<br>Total Individuals: {totalIndividuals}"))
+                )
+            },
+            error=function(cond) {
+                warning(paste0("Error in processing map with the selected inputs: ", cond, "Proceeding!"))
+            })
+
         })
     })
 }
