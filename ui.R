20fccc1d78cf182945084b6d4fce9d6a8d7c34a6
parv-sachdeva <parv.sachdeva@sonomabio.com>
2024-10-06 16:49:27 -0400

    Add name in UI with github link

--- ui.R
+++ ui.R
@@ -3,9 +3,10 @@ ui <- page_sidebar(
   sidebar = sidebar(
     title = "Select Values",
     inputModuleUI("input_module"),
+    h5("Author: ", tags$a(href="https://github.com/parv-sachdeva/", "Parv Sachdeva"), style="position: absolute; bottom: 80px;"),
     img(
       src="https://cdn.prod.website-files.com/6525256482c9e9a06c7a9d3c/6539655f4ca172cfc5deaad8_Appsilon_logo.svg",
-      style="position: absolute; bottom: 10px; "
+      style="position: absolute; bottom: 10px;"
     ),
     open = TRUE
   ),
